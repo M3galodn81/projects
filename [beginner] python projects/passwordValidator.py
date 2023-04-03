ef22652a6a7e252345456a788f0e3e43202467a4
M3galodn81 <dalisdismark0@gmail.com>
2023-04-03 22:34:59 +0800

    Finished Password Validator

--- [beginner] python projects/passwordValidator.py
+++ [beginner] python projects/passwordValidator.py
@@ -21,33 +21,45 @@ for i in password:
 
 # print(password_length)
 
+
 def ratePasswordLength():
+    password_rating = 0
     rate_password_length = ''
     if password_length <= 8:
-        rate_password_length = 'Needs to be longer than 12 characters'
+        rate_password_length = 'Needs to be longer than 12 characters\n'
+        password_rating += 1
     if password_length >= 8 and password_length < 12:
-        rate_password_length = 'Needs to be longer than 12 characters'
+        rate_password_length = 'Needs to be longer than 12 characters\n'
+        password_rating += 2
     if password_length >= 12 and password_length <16:
-        rate_password_length = 'Good enough'
+        rate_password_length = 'Good enough\n'
+        password_rating += 3
     if password_length >= 16:
-        rate_password_length = 'Best'
-    
-    return rate_password_length
+        rate_password_length = 'Best\n'
+        password_rating += 4
+    return rate_password_length,password_rating
 
 def ratePasswordVariety():
+    password_rating = 4
     rate_variety = ''
     if upperCase_numberChar < 1:
         rate_variety += f'No Upper Case Characters \n'
-    if lowerCase_numberChar < 1:
+        password_rating -= 1
+    elif lowerCase_numberChar < 1:
         rate_variety += f'No Lower Case Characters \n'
-    if numbers_numberChar < 1:
+        password_rating -= 1
+    elif numbers_numberChar < 1:
         rate_variety += f'No Numbers Characters \n'
-    if symbols_numberChar < 1:
+        password_rating -= 1
+    elif symbols_numberChar < 1:
         rate_variety += f'No Symbol Characters \n'
-
-    return rate_variety
+        password_rating -= 1
+    else:
+        rate_variety += f'Best variety of characters\n'
+    return rate_variety,password_rating
 
 def ratePasswordComplexity():
+    password_rating = 0
     rate_complexity = ''
     password_type = []
     for char in password:
@@ -59,8 +71,6 @@ def ratePasswordComplexity():
             password_type.append(2)
         if any(i in punctuation for i in char):
             password_type.append(3)
-    # print(password_type)
-
     char_type:int = None
     repeated_type = 0
     for x in range(len(password_type)):
@@ -72,17 +82,20 @@ def ratePasswordComplexity():
         rate_complexity += f'Password has mutliple similar characters'
     if repeated_type >= (0.25 * password_length) and repeated_type < (0.50 * password_length):
         rate_complexity += f'Password is complex enough'
+        password_rating += 1
     if repeated_type < (0.25 * password_length):
         rate_complexity += f'Password is very complex'
+        password_rating += 2
+    return rate_complexity,password_rating
 
-    return rate_complexity
 
+print(f'Length: {ratePasswordLength()[1]} / 4')
+print(ratePasswordLength()[0])
 
-print(f'Length:')
-print(ratePasswordLength())
+print(f'Variety: {ratePasswordVariety()[1]} / 4')
+print(ratePasswordVariety()[0])
 
-print(f'Variety:')
-print(ratePasswordVariety())
+print(f'Complexity: {ratePasswordComplexity()[1]} / 2')
+print(ratePasswordComplexity()[0])
 
-print(f'Complexity:')
-print(ratePasswordComplexity())
+print(f'Rating: {ratePasswordComplexity()[1] + ratePasswordLength()[1] + ratePasswordVariety()[1]} / 10')
